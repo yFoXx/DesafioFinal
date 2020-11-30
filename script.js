9b4cfeff8be25eb8d825435cfa1a2aec9066107e
yFoXx <jaisomhda28@gmail.com>
2020-11-30 19:28:57 -0400

    Correção

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-.kitchen-sink-drawer {
-  $custom-size 500px;
-  @include bmd-drawer-x-out($custom-size);
-  &:not(.bmd-drawer-out){
-    @each $breakpoint in map-keys($grid-breakpoints){
-      @include bmd-drawer-x-in-up($custom-size, $breakpoint);
-    }
-  }
-}
-
 function sharenoticia(titulo, texto, url){
 
   if (navigator.share){
