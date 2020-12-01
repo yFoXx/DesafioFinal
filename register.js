118d61763ead472206bee40bd598c58fb5247ba2
yFoXx <jaisomhda28@gmail.com>
2020-11-30 20:48:27 -0400

    Correção

--- register.js
+++ register.js
@@ -1,10 +1,5 @@
-function registerServiceWorker() {
-  // registrando o service worker para navegadores com suporte
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(() => {
-      console.log('Service Worker registrado com sucesso.');
-    }).catch(error => {
-      console.log('Service Worker falhou:', error);
-    });
-  }
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('sw.js')
+  .then(function(){console.log('serviceWorker está registrado')})
+  .catch(function(){console.log('Erro ao registrar serviceWorker')})
 }
